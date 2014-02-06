169b28757cea06c6bf22a9e633304bddfacd4013
Ryan Pack <ryanpack78@gmail.com>
2014-02-05 20:05:48 -0600

    Renamed main folder.

---
+++ FlickrSearch/MyApp/app/model/Flickr.js
@@ -0,0 +1,4 @@
+Ext.define('MyApp.model.Flickr', {
+	extend: 'Ext.data.Model',
+	fields: ['media', 'author']
+});
---
+++ FlickrSearch/build/temp/production/MyApp/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require C:\Users\Ryan\htdocs\Workspaces\MusicApp\MyApp\app.js
